d75df2bf7520f4b002dee000054abe42625b0063
Nayeem Ur Rehaman <nayeemgithub@gmail.com>
2026-03-04 07:19:09 +0530

    Updated hero section and resume link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -75,14 +75,14 @@ const HeroSection = () => {
                       "cursor-default font-display sm:text-xl md:text-xl whitespace-nowrap bg-clip-text "
                     )}
                   >
-                    A Full Stack Web Developer
+                    AI/ML Engineer | Automotive Computer Vision
                   </p>
                 </BlurIn>
               </div>
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1MTSsUA8V7Po2AsNXT8kZ5sLOpzC8l7qm/view?usp=sharing"
+                    "https://drive.google.com/file/d/1AMSPNAk-nOChirdyWpvD6jFz97E0RSjG/view?usp=drivesdk"
                   }
                   target="_blank"
                   className="flex-1"
